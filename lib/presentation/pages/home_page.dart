673c4afc1ee94ca09dcd1684d8889ca0c54835e3
Ruan <ruanpalhares489@gmail.com>
2023-08-24 20:43:30 -0300

    branch com o código da aula do dia Thu Aug 24 20:43:30     2023

---
+++ lib/presentation/pages/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:automcao_residencial/presentation/pages/help_page.dart';
+import 'package:flutter/material.dart';
+
+bool goodtogo = true;
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Iluminação Residencial'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Row(),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 150),
+                child: ElevatedButton(
+                  onPressed: () => buttonDelay(context),
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(400, 400),
+                      shape: const CircleBorder()),
+                  child: Image.asset('image/light.png'),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 60, left: 35),
+                  child: SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HelpPage(),
+                            ));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black12,
+                          fixedSize: const Size(100, 100),
+                          shape: const CircleBorder()),
+                      child: Image.asset('image/interr.png'),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void buttonDelay(BuildContext context) {
+    if (!goodtogo) {
+      return;
+    }
+    if (goodtogo) {
+      var message = '';
+      bool conexao = false;
+      if (conexao == true) {
+        message = 'Conexão estabelecida!';
+      } else {
+        message = 'Conexão não encontrada!';
+      }
+      goodtogo = false;
+
+      var snackbar = SnackBar(content: Text(message));
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+
+      Future.delayed(const Duration(milliseconds: 5000), () {
+        goodtogo = true;
+      });
+    }
+  }
+}
